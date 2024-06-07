b545a6f1da7abe6e2d029e2c5f0281f61e14fed0
Koto <indrakoto@gmail.com>
2024-06-07 23:23:52 +0700

    update design menu navbar dan sidebar

--- resources/views/layouts/opd/app.blade.php
+++ resources/views/layouts/opd/app.blade.php
@@ -73,10 +73,9 @@
         <!-- /.content-wrapper -->
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                <b>Version</b> 1.0.0
+                <b>Form Pencegahan Online</b>
             </div>
-            <strong>Copyright &copy; 2022 <a href="/">Bawaslu</a></strong> - All rights
-            reserved.
+            <strong><a href="/">BAWASLU</a> &copy; 2022 </strong>
         </footer>
 
         <!-- Control Sidebar -->
--- resources/views/layouts/opd/navbar.blade.php
+++ resources/views/layouts/opd/navbar.blade.php
@@ -4,62 +4,15 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-
+        <li class="nav-item"><a href="/welcome" class="nav-link">Form Pencegahan Online</a></li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
 
+        <li class="nav-item"><a href="/profil" class="nav-link"><i class="fas fa-user mr-2"></i>{{ Auth::user()->name }}</a></li>
 
-        <!-- Messages Dropdown Menu -->
 
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="fas fa-user mr-2"></i>
-                <span class="badge badge-warning navbar-badge"></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">
-                   <h4> <a href="/profil"> Profil User</span></h4></a>
-                <div class="dropdown-divider"></div>
-                {{--  <a href="#" class="dropdown-item">
-                    <i class="fas fa-envelope mr-2"></i> {{ Auth::user()->Email }}
-                </a>  --}}
-                <div class="dropdown-divider"></div>
-                {{--  <a href="" class="dropdown-item">
-                    <i class="fas fa-user mr-2"></i> {{ Auth::user()->name }}
-                </a>  --}}
-                <div class="dropdown-divider"></div>
-                {{--  <a href="#" class="dropdown-item">
-          <i class="fas fa-file mr-2"></i> 3 new reports
-          <span class="float-right text-muted text-sm">2 days</span>
-        </a>  --}}
-                <div class="dropdown-divider"></div>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                              this.closest('form').submit();">
-                        <span class="dropdown-item dropdown-footer">
-                            <h4> Logout</h4>
-                        </span>
-                    </x-dropdown-link>
-                </form>
-                {{--  <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>  --}}
-            </div>
-        </li>
-     
-        <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                <i class="fas fa-expand-arrows-alt"></i>
-            </a>
-        </li>
-        {{--  <li class="nav-item">
-      <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-        <i class="fas fa-th-large"></i>
-      </a>
-    </li>  --}}
     </ul>
 </nav>
