48683c3bff4fb2b6b4afa087e5cf4e765d412800
Koto <indrakoto@gmail.com>
2024-10-18 20:27:52 +0700

    update system

--- config/captcha.php
+++ config/captcha.php
@@ -26,7 +26,7 @@ return [
         'height' => 35,
         'quality' => 90,
         'lines' => 5,
-        'bgImage' => false,
+        'bgImage' => true,
         'bgColor' => '#ffffff',
         'fontColors' => ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
         'contrast' => -5,
