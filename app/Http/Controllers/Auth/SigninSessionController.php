8ead5863c520e8873e4bfa899f8b6598feba0ae9
Koto <indrakoto@gmail.com>
2024-10-23 23:34:48 +0700

    update form login

--- app/Http/Controllers/Auth/SigninSessionController.php
+++ app/Http/Controllers/Auth/SigninSessionController.php
@@ -11,14 +11,21 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
 
+use App\Mail\ResetPasswordMail;
+use App\Models\PasswordResetToken;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+
 class SigninSessionController extends Controller
 {
     /**
      * Display the login view.
      */
-    public function create(): View
+    public function create()
     {
-        return view('auth.signin');
+        return view('auth.signin_open');
+        //return view('auth.signin_close');
     }
 
     /**
@@ -53,5 +60,85 @@ class SigninSessionController extends Controller
 
     public function reloadCaptcha(){
         return response()->json(['captcha'=>captcha_img('flat')]);
+    }
+
+
+    public function forgot_password()
+    {
+        return view('auth.forgot-password');
+    }
+
+    public function forgot_password_act(Request $request)
+    {
+        $customMessage = [
+            'email.required'    => 'Email tidak boleh kosong',
+            'email.email'       => 'Email tidak valid',
+            'email.exists'      => 'Email tidak terdaftar di database',
+        ];
+
+        $request->validate([
+            'email' => 'required|email|exists:users,email'
+        ], $customMessage);
+
+        $token = \Str::random(60);
+
+        PasswordResetToken::class;
+        PasswordResetToken::updateOrCreate(
+            [
+                'email' => $request->email
+            ],
+            [
+                'email' => $request->email,
+                'token' => $token,
+                'created_at' => now(),
+            ]
+        );
+
+        Mail::to($request->email)->send(new ResetPasswordMail($token));
+
+        return redirect()->route('forgot-password')->with('success', 'Kami telah mengirimkan link reset password ke email anda');
+    }
+
+    public function validasi_forgot_password_act(Request $request)
+    {
+        $customMessage = [
+            'password.required' => 'Password tidak boleh kosong',
+            'password.min'      => 'Password minimal 6 karakter',
+        ];
+
+        $request->validate([
+            'password' => 'required|min:6'
+        ], $customMessage);
+
+        $token = PasswordResetToken::where('token', $request->token)->first();
+
+        if (!$token) {
+            return redirect()->route('login')->with('failed', 'Token tidak valid');
+        }
+
+        $user = User::where('email', $token->email)->first();
+
+        if (!$user) {
+            return redirect()->route('login')->with('failed', 'Email tidak terdaftar di database');
+        }
+
+        $user->update([
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token->delete();
+
+        return redirect()->route('login')->with('success', 'Password berhasil direset');
+    }
+
+    public function validasi_forgot_password(Request $request, $token)
+    {
+        $getToken = PasswordResetToken::where('token', $token)->first();
+
+        if (!$getToken) {
+            return redirect()->route('login')->with('failed', 'Token tidak valid');
+        }
+
+        return view('auth.validasi-token', compact('token'));
     }
 }
